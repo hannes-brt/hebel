a59394224af7005b90f135d34a6f88f71b6f055c
Hannes Bretschneider <hannes@psi.utoronto.ca>
2013-05-26 16:01:36 -0400

    implemented strided convolution

--- neural_nets/pycuda_ops/convolution.py
+++ neural_nets/pycuda_ops/convolution.py
@@ -8,6 +8,8 @@ MAX_NUM_FILTERS = 100
 assert CONV_BLOCK_SIZE[0] == CONV_BLOCK_SIZE[1]
 
 code = """
+#define CEILING(x) (int)(x) + (1 - (int)((int)((x) + 1) - (x)))
+
 #define TILE_SIZE %(TILE_SIZE)d
 #define MAX_WIDTH_FILTER %(MAX_WIDTH_FILTER)d
 #define MAX_NUM_FILTERS %(MAX_NUM_FILTERS)d
@@ -99,14 +101,18 @@ __global__ void conv1d_matrix(%(data_type)s *input, %(data_type)s *target, %(dat
     }
 }
 
-__global__ void conv1d_matrix_mult_filter(%(data_type)s *input, %(data_type)s *target, %(data_type)s *filter,
-    unsigned int width, unsigned int height, unsigned int filter_width, unsigned int n_filters) {
+__global__ void conv1d_matrix_mult_filter(const %(data_type)s *input,
+    %(data_type)s *target, const %(data_type)s *filter,
+    const unsigned int width, const unsigned int height,
+    const unsigned int filter_width, const unsigned int n_filters,
+    const unsigned int stride) {
     
     const unsigned int i = blockIdx.y*blockDim.y+threadIdx.y;
     const unsigned int j = blockIdx.x*blockDim.x+threadIdx.x;
     const unsigned int lin_idx = i*width+j;
     const unsigned int row_start = i*width;
-    unsigned int shared_idx, input_idx;
+    const unsigned int target_width = CEILING((double) width / stride);
+    unsigned int shared_idx, input_idx;    
     
     const unsigned int shared_width = TILE_SIZE+MAX_WIDTH_FILTER-1;
     __shared__ %(data_type)s input_shared[TILE_SIZE*(shared_width)];
@@ -139,7 +145,7 @@ __global__ void conv1d_matrix_mult_filter(%(data_type)s *input, %(data_type)s *t
     __syncthreads();
   
     unsigned int filter_idx, target_idx;
-    if (i < height && j < width) {
+    if (!(j%%stride) && i < height && j < width) {
         for (int f=0; f < n_filters; f++) {
             %(data_type)s Pvalue = 0.;
             for (int k=0; k < filter_width; k++) {
@@ -147,7 +153,7 @@ __global__ void conv1d_matrix_mult_filter(%(data_type)s *input, %(data_type)s *t
                 filter_idx = f*filter_width+k;
                 Pvalue += input_shared[shared_idx]*filter[filter_idx];
             }
-            target_idx = f*width*height+i*width+j;
+            target_idx = f*target_width*height+i*target_width+j/stride;
             target[target_idx] = Pvalue;
         }        
     }
@@ -205,14 +211,14 @@ def conv1d_matrix(mat, conv_filter, target=None, stream=None):
         
     return target
 
-def conv1d_matrix_mult_filter(mat, conv_filter, target=None, stream=None):
+def conv1d_matrix_mult_filter(mat, conv_filter, stride=1, target=None, stream=None):
     dtype = mat.dtype
     assert dtype in (np.float32, np.float64)
     assert conv_filter.dtype == dtype
     
     if target is not None:
         assert target.dtype == dtype
-        assert mat.shape == target.shape[:2]
+        assert mat.shape // stride == target.shape[:2]
 
     # import pudb
     # pudb.set_trace()
@@ -226,7 +232,8 @@ def conv1d_matrix_mult_filter(mat, conv_filter, target=None, stream=None):
             1)
     
     if target is None:
-        target = gpuarray.empty((n_filters, height, width),
+        target_width = int(np.ceil(width / float(stride)))
+        target = gpuarray.empty((n_filters, height, target_width),
                                 dtype=dtype)
 
     if dtype == np.float32:
@@ -240,6 +247,7 @@ def conv1d_matrix_mult_filter(mat, conv_filter, target=None, stream=None):
            np.int32(width), np.int32(height),
            np.int32(width_filter),
            np.int32(n_filters),
+           np.int32(stride),
            block=block, grid=grid,
            stream=stream)
         
--- neural_nets_test.py
+++ neural_nets_test.py
@@ -70,7 +70,7 @@ class TestConvolutionMatrix(unittest.TestCase):
 
 class TestConvolutionMatrixMultFilters(unittest.TestCase):
     @staticmethod
-    def cpu_conv1d(x, w):
+    def cpu_conv1d(x, w, stride=1):
         n, m = x.shape        
         n_filters, filter_width = w.shape
         pad_width = filter_width // 2
@@ -78,26 +78,34 @@ class TestConvolutionMatrixMultFilters(unittest.TestCase):
         x_padded = np.concatenate((np.zeros((n, pad_width), dtype=np.float64),
                                    x,
                                    np.zeros((n, pad_width), dtype=np.float64)), 1)
-        y = np.empty((n_filters, x.shape[0], x.shape[1]), dtype=x.dtype)
+        m_output = x.shape[1] // stride
+        y = np.empty((n_filters, x.shape[0], m_output), dtype=x.dtype)
+        # print x.shape, y.shape, stride
         # import pudb
         # pudb.set_trace()
         for f in range(n_filters):
-            for i in range(m):                
-                y[f,:,i] = (x_padded[:,i:(i+filter_width)]*w[f,:]).sum(1)
+            for i in range(0, m_output):
+                ii = i*stride
+                y[f,:,i] = (x_padded[:,ii:(ii+filter_width)]*w[f,:]).sum(1)
+        return y
+
+
+    @staticmethod    
+    def gpu_conv1d(x, w, dtype, stride=1):
+        y = conv1d_matrix_mult_filter(x, w, stride=stride)
         return y
     
-    def conv1d_test_setup(self, height, width, filter_width, n_filters):
+    def conv1d_test_setup(self, height, width, filter_width, n_filters, stride=1):
         for dtype, err_tol in ((np.float32, FLOAT_ERR_TOL), 
                                (np.float64, DOUBLE_ERR_TOL)):
             x = curand((height, width), dtype)
             w = curand((n_filters, filter_width), dtype=dtype)
-
-            y = conv1d_matrix_mult_filter(x, w)
-            y_np = self.cpu_conv1d(x.get(), w.get())
+            y = self.gpu_conv1d(x, w, dtype, stride)
+            y_np = self.cpu_conv1d(x.get(), w.get(), stride=stride)
             y_cpu = y.get()
             
             for f in range(n_filters):
-                self.assertLess(np.linalg.norm(y_cpu[:,:,f]-y_np[:,:,f], np.inf), err_tol)
+                self.assertLess(np.linalg.norm(y_cpu[f,:,:]-y_np[f,:,:], np.inf), err_tol)
     
     def test_conv1_matrix_small(self):
         for i in range(100):
@@ -135,6 +143,27 @@ class TestConvolutionMatrixMultFilters(unittest.TestCase):
             w = np.random.randint(5, 15)
             n_filters = np.random.randint(2, 5)            
             self.conv1d_test_setup(n, m, w, n_filters)
+
+    def test_conv1_matrix_stride(self):
+        for i in range(100):
+            n = np.random.randint(200, 1000)
+            m = np.random.randint(200, 1000)
+            filter_width = 5
+            n_filters = np.random.randint(2, 5)
+            stride = np.random.randint(2, 5)
+
+            for dtype, err_tol in ((np.float32, FLOAT_ERR_TOL), 
+                                   (np.float64, DOUBLE_ERR_TOL)):
+                x = curand((n, m), dtype)
+                w = curand((n_filters, filter_width), dtype=dtype)
+
+                y_no_stride = self.gpu_conv1d(x, w, dtype, 1)
+                y_stride = self.gpu_conv1d(x, w, dtype, stride)
+
+                for f in range(n_filters):                
+                    self.assertLess(np.linalg.norm(y_no_stride.get()[f,:,::stride]-
+                                                   y_stride.get()[f,:,:],
+                                    np.inf), err_tol)
 
 if __name__ == '__main__':
     unittest.main()
